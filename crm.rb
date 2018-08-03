cf96e645b6e51ba58257f6cfbcbf0bb896fc3723
curtischatch <curtis.chat.hk@gmail.com>
2018-08-03 15:11:42 -0400

    created contacts

--- crm.rb
+++ crm.rb
@@ -4,3 +4,16 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/about' do
+  erb :about
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
